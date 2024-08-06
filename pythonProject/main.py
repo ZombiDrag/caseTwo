94c7d2cbd7cf4162351ec38248ba0b2b2bd09b89
ZombiDrag <77924677+ZombiDrag@users.noreply.github.com>
2024-08-06 08:00:23 +0300

    Add files via upload

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -5,7 +5,7 @@ import time
 def text_slow(a):
     for i in a:
         print(i, end='',flush=True)
-        time.sleep(0.07)
+        time.sleep(0.05)
     return ''
 
 # Основная логика приложения. Создает переменную ' n ' и в дальнейшем сравнивает
@@ -15,24 +15,31 @@ def rendom_number():
     n = random.randrange(0, 101)
 
     print("Добро пожаловать в игру угадай чесло!")
-    for i in range(1, 5):
-        f = int(input(text_slow('Введи число:  -->')))
-        if f == n:
-            text_slow("Поздравляю! Вы выиграли!")
-            time.sleep(20)
-            break
-        elif i == 4:
-            print(f"Вы не угадали.\nОсталось попыток: {4 - i}")
-            time.sleep(20)
-            break
-
-        else:
-            if f > n:
-                text_slow(f"Вы не угадали попробуйте уменьшить число.\nОсталось попыток: {4 - i}. ")
+    i = 4
+    while True:
+        try:
+            f = int(input(text_slow('Введи число:  -->')))
+            if f == n:
+                text_slow("Поздравляю! Вы выиграли!")
+                time.sleep(20)
+                break
+            elif i == 0:
+                print(f"Вы не угадали.\nОсталось попыток: {i}")
+                time.sleep(20)
+                break
 
             else:
-                text_slow(f"Вы не угадали попробуйте увеличить число.\nОсталось попыток: {4 - i}. ")
+                if f > n:
 
+                    text_slow(f"Вы не угадали попробуйте уменьшить число.\nОсталось попыток: {i}. ")
+                    i -= 1
+
+                else:
+
+                    text_slow(f"Вы не угадали попробуйте увеличить число.\nОсталось попыток: {i}. ")
+                    i -= 1
+        except ValueError:
+            text_slow("Неверное значение попробуйте ввести целое число.\n")
 
 
 if __name__ == "__main__":
